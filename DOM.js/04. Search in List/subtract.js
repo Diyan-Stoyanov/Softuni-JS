cee2c01adc0c4be2fed33e9e4f4a3003e1bdd0c7
Diyan-Stoyanov <76534825+Diyan-Stoyanov@users.noreply.github.com>
2021-03-12 17:39:48 +0200

    Delete subtract.js

--- DOM.js/04. Search in List/subtract.js
+++
@@ -1,8 +0,0 @@
-function subtract() {
-    let first = Number(document.getElementById('firstNumber').value);
-    let second = Number(document.getElementById('secondNumber').value);
-    let res = first - second;
-   
-    document.getElementById('result').textContent = res;
-console.log(res);
-}
